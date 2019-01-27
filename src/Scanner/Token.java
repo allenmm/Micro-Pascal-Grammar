7b5aa8ffa0a1b6a045d9d09bf9daa1f8f4109f19
Marissa Allen <allenmm@augsburg.edu>
2019-01-27 16:24:55 -0600

    Added getters and setters for the Token class. And javadocs.

--- src/Scanner/Token.java
+++ src/Scanner/Token.java
@@ -6,11 +6,36 @@
  */
 package scanner;
 
-public class Token {
+public class Token
+{
     public String lexeme;
     public TokenType type;
-    public Token( String l, TokenType t) {
+    
+	/**
+	 * Constructor that creates a Token that has a lexeme as the name and
+	 * a type from the TokenType enum class.
+	 *
+	 * @param l - The Token name defined by the characters 
+	 * in the input stream.
+	 * @param t - The Token type.
+	 */
+    public Token( String l, TokenType t)
+    {
         this.lexeme = l;
         this.type = t;
     }
+    
+	public String getLexeme() {
+		return lexeme;
+	}
+
+	public TokenType getType() {
+		return type;
+	}
+
+	@Override
+	public String toString() {
+		return "Token [lexeme=" + lexeme + ", type=" + type + ", toString()=" + super.toString() + "]";
+	}
+	
 }
