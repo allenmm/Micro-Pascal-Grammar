d5163bfb20dad542b6ca3bfb5d0f25b8d8f738c2
Marissa Allen <allenmm@augsburg.edu>
2019-01-28 19:56:06 -0600

    Added testing for 'foo' to testnextToken() to see if it the type matches the TokenType.

--- src/scanner/ScannerTest.java
+++ src/scanner/ScannerTest.java
@@ -66,5 +66,22 @@ public class ScannerTest {
 
             System.out.println("Unexpected Token found: " + testToken);
         }
+
+        System.out.println("Token test 'foo' from file: ");
+        //Expected enum Token type.
+        expected = TokenType.ID;
+
+        //Testing the ID Token to see if it passes.
+        try {
+            testToken = scanner.nextToken();
+            //The actual Token type.
+            TokenType actual = testToken.getType();
+            //Testing to see if the type actually matches the TokenType enum.
+            assertEquals(expected, actual);
+            System.out.println("foo passed.");
+        } catch (Exception e) {
+
+            System.out.println("Unexpected Token found: " + testToken);
+        }
     }
 }
