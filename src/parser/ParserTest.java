08e0a4214dd6c6adbfd2d541d66445d20c2f29e4
Marissa Allen <allenmm@augsburg.edu>
2019-02-11 12:23:14 -0600

    Uncommented other test functions, replaced the null data for each of them with an actual parser constructor containing data values, removed the fail function and replaced it with a system.out.println comment. Updated the javadocs.

--- src/parser/ParserTest.java
+++ src/parser/ParserTest.java
@@ -11,8 +11,8 @@ import scanner.TokenType;
  * A Java Program that illustrates reading from a text file or strings
  * using a parser class using JUnit testing. Either a string or a file
  * path name will be passed into the parser constructor from the parser
- * class. If the string file path is present and the file name is true,
- * then the test will pass the file path
+ * class and grab the . If the string file path is present and the file name is true,
+ * then the JUnit test will pass the file path,  then the
  *
  * @author Marissa Allen
  */
@@ -30,7 +30,10 @@ public class ParserTest {
     }
 
     /**
-     * Test of exp method, of class Parser.
+     * This method uses JUnit to test the exp method from the Parser
+     * class by testing to see if the current token matches the
+     * expected token type in the exp method. This is a text file path
+     * test.
      */
     @Test
     public void testExp() {
@@ -43,7 +46,10 @@ public class ParserTest {
     }
 
     /**
-     * Test of exp_prime method, of class Parser.
+     * This method uses JUnit to test the exp_prime method from the Parser
+     * class by testing to see if the current token matches the
+     * expected token type in the exp_prime method. This is a text string
+     * test.
      */
     @Test
     public void testExp_prime() {
@@ -55,78 +61,77 @@ public class ParserTest {
         System.out.println("It Parsed!");
     }
 
-//    /**
-//     * Test of addop method, of class Parser.
-//     */
-//    @Test
-//    public void testAddop() {
-//        System.out.println("\n" + "######################" + "\n" +
-//                    "#   Test addop  #" + "\n" +
-//                    "######################" + "\n");
-//        Parser instance = null;
-//        instance.addop();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
     /**
-     * Test of term method, of class Parser.
+     * This method uses JUnit to test the addop method from the Parser
+     * class by testing to see if the current token matches the
+     * expected token type in the addop method. This is a text string
+     * test.
+     */
+    @Test
+    public void testAddop() {
+        System.out.println("\n" + "######################" + "\n" +
+                    "#   Test addop  #" + "\n" +
+                    "######################" + "\n");
+        TokenType plus = TokenType.PLUS;
+        Parser instance = new Parser( "+", false);
+        instance.match(plus);
+        System.out.println("Recognized the single addop.");
+    }
+
+    /**
+     * This method uses JUnit to test the term method from the Parser
+     * class by testing to see if the current token matches the
+     * expected token type in the term method. This is a text string
+     * test.
      */
     @Test
     public void testTerm() {
         System.out.println("\n" + "######################" + "\n" +
-                "#   Test term   #" + "\n" +
+                "#     Test term      #" + "\n" +
                 "######################" + "\n");
         Parser instance = new Parser("23 / 17", false);
         instance.term();
-        System.out.println("Parsed a term");
+        System.out.println("Parsed a term.");
     }
 
-//    /**
-//     * Test of term_prime method, of class Parser.
-//     */
-//    @Test
-//    public void testTerm_prime() {
-//       System.out.println("\n" + "######################" + "\n" +
-//       "#   Test term_prime   #" + "\n" +
-//       "######################" + "\n");
-//        Parser instance = null;
-//        instance.term_prime();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of mulop method, of class Parser.
-//     */
-//    @Test
-//    public void testMulop() {
-//       System.out.println("\n" + "######################" + "\n" +
-//       "#   Test mulop   #" + "\n" +
-//       "######################" + "\n");
-//        Parser instance = new Parser( "expressions/muloponly.pas");
-//        //instance.mulop();
-//        System.out.println("Saw the single mulop");
-//    }
-//
+    /**
+     *This method uses JUnit to test the term_prime method from the Parser
+     * class by testing to see if the current token matches the
+     * expected token type in the term_prime method. This is a text
+     * string test.
+     */
+    @Test
+    public void testTerm_prime() {
+        System.out.println("\n" + "######################" + "\n" +
+                "#   Test term_prime   #" + "\n" +
+                "######################" + "\n");
+        Parser instance = new Parser( "* foo /", false);
+        instance.term_prime();
+        System.out.println("Parsed a term.");
+    }
+
 
     /**
-     * Test of mulop method, of class Parser.
+     * This method uses JUnit to test the mulop method from the Parser
+     * class by testing to see if the current token matches the
+     * expected token type in the mulop method. This is a text string
+     * test.
      */
     @Test
     public void testMulop() {
-       System.out.println("\n" + "######################" + "\n" +
-       "#   Test mulop   #" + "\n" +
-       "######################" + "\n");
+        System.out.println("\n" + "######################" + "\n" +
+                "#     Test mulop     #" + "\n" +
+                "######################" + "\n");
         Parser instance = new Parser( "*", false);
         instance.mulop();
-        System.out.println("Recognized the single mulop");
+        System.out.println("Recognized the single mulop.");
     }
 
     /**
-     * This method uses JUnit to test the factor method from the Parser
+     *This method uses JUnit to test the factor method from the Parser
      * class by testing to see if the current token matches the
-     * expected token type.
+     * expected token type in the factor method. This is a text string
+     * test.
      */
     @Test
     public void testFactor() {
@@ -135,34 +140,40 @@ public class ParserTest {
                 "######################" + "\n");
         Parser instance = new Parser( "87654321", false);
         instance.factor();
+        System.out.println("Recognized the factor token.");
     }
-//
-//    /**
-//     * This method uses JUnit to test the match method from the Parser
-//     * class.
-//     */
-//    @Test
-//    public void testMatch() {
-//        System.out.println("\n" + "match");
-//        TokenType ett = null;
-//        Parser instance = null;
-//        instance.match(ett);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * This method uses JUnit to test the error method from the Parser
-//     * class.
-//     */
-//    @Test
-//    public void testError() {
-//        System.out.println("\n" + "error");
-//        String message = "";
-//        Parser instance = null;
-//        instance.error(message);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
 
+    /**
+     *This method uses JUnit to test the match method from the Parser
+     * class by testing to see if the current token matches the
+     * expected token type in the match method. This is a text string
+     * test that tests the period token type as the expected token to
+     * match against the string token passed into the parser.
+     */
+    @Test
+    public void testMatch()
+    {
+        System.out.println("\n" + "######################" + "\n" +
+                "#     Test match     #" + "\n" +
+                "######################" + "\n");
+        TokenType ett = TokenType.PERIOD;
+        Parser instance = new Parser( ".", false);
+        instance.match(ett);
+        System.out.println("It matches!");
+    }
+
+    /**
+     * This method uses JUnit to test the error method from the Parser
+     * class.
+     */
+    @Test
+    public void testError() {
+        System.out.println("\n" + "######################" + "\n" +
+                "#     Test error     #" + "\n" +
+                "######################" + "\n");
+        String message = ", error test";
+        Parser instance = new Parser( "", false);
+        instance.error(message);
+        System.out.println("Successfully tested the error.");
+    }
 }
