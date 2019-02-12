fcb06260d57994fa480bc8850d952e9f1eba7dda
Marissa Allen <allenmm@augsburg.edu>
2019-02-12 14:29:48 -0600

    Removed a duplicate method, exp(). Rearranged the methods into micro pascal grammatical order. Added javadocs to a function. Changed the assignop function to have the correct condition statement.

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -78,182 +78,6 @@ public class Parser {
     ///////////////////////////////
 
     /**
-     * This method is used to parse an expression. It is the
-     * simple expression in the micro pascal grammar.
-     */
-    public void exp()
-    {
-       if(lookahead.getType() == TokenType.ID ||
-               lookahead.getType() == TokenType.NUMBER ||
-               lookahead.getType() == TokenType.LPAREN ||
-               lookahead.getType() == TokenType.NOT)
-       {
-           term();
-           simple_part();
-       }
-       else if(lookahead.getType() == TokenType.PLUS ||
-               lookahead.getType() == TokenType.MINUS)
-       {
-           sign();
-           term();
-           simple_part();
-       }
-       else
-       {
-           error("Simple Expression");
-       }
-    }
-
-    /**
-     * This is 
-     */
-    public void simple_part()
-    {
-        if( lookahead.getType() == TokenType.PLUS ||
-                lookahead.getType() == TokenType.MINUS ||
-                lookahead.getType() == TokenType.OR) {
-            addop();
-            term();
-            simple_part();
-        }
-        else{
-            // Do nothing. The empty lambda option.
-        }
-    }
-
-    /**
-     *
-     */
-    public void addop() {
-        if( lookahead.getType() == TokenType.PLUS)
-        {
-            match( TokenType.PLUS);
-        }
-        else if( lookahead.getType() == TokenType.MINUS)
-        {
-            match( TokenType.MINUS);
-        }
-        else if ( lookahead.getType() == TokenType.OR)
-        {
-            match( TokenType.OR); //need to add OR here?
-        }
-        else {
-            error( "Addop");
-        }
-    }
-
-    /**
-     *
-     */
-    public void term() {
-        factor();
-        term_part();
-    }
-
-    /**
-     *
-     */
-    public void term_part()
-    {
-        if( isMulop( lookahead) )
-        {
-            mulop();
-            factor();
-            term_part();
-        }
-        else
-        {
-            //Do nothing. The empty lambda option.
-        }
-    }
-
-    /**
-     * Checks to see if the token from the input string is a mulop token
-     * or not and returns a boolean expression if it's true or false.
-     * @param token - The token to check against from the input string.
-     * @return - If the token is a mulop it will be returned, otherwise
-     * it's false and nothing will be returned.
-     */
-    private boolean isMulop( Token token) {
-        boolean answer = false;
-        if( token.getType() == TokenType.MULTI ||
-                token.getType() == TokenType.FSLASH ||
-                token.getType() == TokenType.DIV ||
-                token.getType() == TokenType.MOD ||
-                token.getType() == TokenType.AND) {
-            answer = true;
-        }
-        return answer;
-    }
-
-    /**
-     *
-     */
-    public void mulop() {
-        if( lookahead.getType() == TokenType.MULTI)
-        {
-            match( TokenType.MULTI);
-        }
-        else if( lookahead.getType() == TokenType.FSLASH)
-        {
-            match( TokenType.FSLASH);
-        }
-        else if( lookahead.getType() == TokenType.DIV)
-        {
-            match( TokenType.DIV);
-        }
-        else if( lookahead.getType() == TokenType.MOD)
-        {
-            match( TokenType.MOD);
-        }
-        else if( lookahead.getType() == TokenType.AND)
-        {
-            match( TokenType.AND);
-        }
-        else {
-            error( "Mulop");
-        }
-    }
-
-    /**
-     *
-     *
-     * @param expected - This is the expected token type.
-     */
-    public void match( TokenType expected) {
-        System.out.println("match( " + expected + ")");
-        if( this.lookahead.getType() == expected) {
-            try {
-                this.lookahead = scanner.nextToken();
-                if( this.lookahead == null) {
-                    this.lookahead = new Token( "End of File", null);
-                }
-            } catch (IOException ex) {
-                error( "Scanner exception");
-            }
-        }
-        else {
-            error("Match of " + expected + " found " + this.lookahead.getType()
-                    + " instead.");
-        }
-    }
-
-    /**
-     * A custom error method for the parser. When the parser errors, an
-     * error message is printed out and the program is exited. The error
-     * message contains the error message string from the specific
-     * function, the line number the error was on, and the column number.
-     * @param message - The error message that is printed by using the
-     * error strings from different functions.
-     */
-    public void error( String message) {
-        System.out.println( "Error " + message + " at line " +
-                this.scanner.getLine() + " column " +
-                this.scanner.getColumn());
-        //System.exit( 1);
-    }
-
-    /**
      * Recognizes a pascal program.
      */
     public void program()
@@ -307,6 +131,7 @@ public class Parser {
             //Do nothing. The empty lambda option.
         }
     }
+
 
     /**
      *
@@ -498,24 +323,6 @@ public class Parser {
         }
     }
 
-
-    /**
-     * Creating the assignop method stated in the grammar.
-     * According to the micro pascal grammar, the ASSIGN token type
-     * stands for the assignop := token.
-     */
-    public void assignop()
-    {
-        if(lookahead.getType() == TokenType.ID)
-        {
-            match(TokenType.ASSIGN);
-        }
-        else
-        {
-            error("Assignop");
-        }
-    }
-
     /**
      *
      */
@@ -523,9 +330,9 @@ public class Parser {
     {
         if(lookahead.getType() == TokenType.ID)
         {
-                variable();
-                assignop();
-                expression();
+            variable();
+            assignop();
+            expression();
         }
                /* procedure_statement() path goes here.
                This path is future work we don't have to do yet.
@@ -650,65 +457,8 @@ public class Parser {
     }
 
     /**
-     * Checks to see if the token from the input string is a relop token
-     * or not and returns a boolean expression if it's true or false.
-     * @param token - The lookahead token to check against from the
-     * input string.
-     * @return - If the token is a relop it will be returned, otherwise
-     * it's false and nothing will be returned.
-     */
-    private boolean isRelop( Token token)
-    {
-        boolean answer = false;
-        if(token.getType() == TokenType.EQUIV ||
-                token.getType() == TokenType.NOTEQUAL ||
-                token.getType() == TokenType.LTHAN ||
-                token.getType() == TokenType.GTHAN ||
-                token.getType() == TokenType.LTHANEQUAL ||
-                token.getType() == TokenType.GTHANEQUAL)
-        {
-            answer = true;
-        }
-        return answer;
-    }
-
-    /**
-     *
-     */
-    public void relop()
-    {
-        if(this.lookahead.getType() == TokenType.EQUIV)
-        {
-            match(TokenType.EQUIV);
-        }
-        else if(this.lookahead.getType() == TokenType.NOTEQUAL)
-        {
-            match(TokenType.NOTEQUAL);
-        }
-        else if(this.lookahead.getType() == TokenType.LTHAN)
-        {
-            match(TokenType.LTHAN);
-        }
-        else if(this.lookahead.getType() == TokenType.GTHAN)
-        {
-            match(TokenType.GTHAN);
-        }
-        else if(this.lookahead.getType() == TokenType.LTHANEQUAL)
-        {
-            match(TokenType.LTHANEQUAL);
-        }
-        else if(this.lookahead.getType() == TokenType.GTHANEQUAL)
-        {
-            match(TokenType.GTHANEQUAL);
-        }
-        else
-        {
-            error("Relop");
-        }
-    }
-
-    /**
-     *
+     * This method is used to parse an expression. It is the
+     * simple expression in the micro pascal grammar.
      */
     public void simple_expression()
     {
@@ -730,6 +480,48 @@ public class Parser {
         else
         {
             error("Simple Expression");
+        }
+    }
+
+    /**
+     * This is 
+     */
+    public void simple_part()
+    {
+        if( lookahead.getType() == TokenType.PLUS ||
+                lookahead.getType() == TokenType.MINUS ||
+                lookahead.getType() == TokenType.OR) {
+            addop();
+            term();
+            simple_part();
+        }
+        else{
+            // Do nothing. The empty lambda option.
+        }
+    }
+
+    /**
+     *
+     */
+    public void term() {
+        factor();
+        term_part();
+    }
+
+    /**
+     *
+     */
+    public void term_part()
+    {
+        if( isMulop( lookahead) )
+        {
+            mulop();
+            factor();
+            term_part();
+        }
+        else
+        {
+            //Do nothing. The empty lambda option.
         }
     }
 
@@ -798,6 +590,192 @@ public class Parser {
         {
             error("Sign");
         }
+    }
+
+    /**
+     * This method is used to compare the lookahead token with
+     * a token type to see if it matches the same type.
+     */
+    public void relop()
+    {
+        if(this.lookahead.getType() == TokenType.EQUIV)
+        {
+            match(TokenType.EQUIV);
+        }
+        else if(this.lookahead.getType() == TokenType.NOTEQUAL)
+        {
+            match(TokenType.NOTEQUAL);
+        }
+        else if(this.lookahead.getType() == TokenType.LTHAN)
+        {
+            match(TokenType.LTHAN);
+        }
+        else if(this.lookahead.getType() == TokenType.GTHAN)
+        {
+            match(TokenType.GTHAN);
+        }
+        else if(this.lookahead.getType() == TokenType.LTHANEQUAL)
+        {
+            match(TokenType.LTHANEQUAL);
+        }
+        else if(this.lookahead.getType() == TokenType.GTHANEQUAL)
+        {
+            match(TokenType.GTHANEQUAL);
+        }
+        else
+        {
+            error("Relop");
+        }
+    }
+
+    /**
+     *
+     */
+    public void addop() {
+        if( lookahead.getType() == TokenType.PLUS)
+        {
+            match( TokenType.PLUS);
+        }
+        else if( lookahead.getType() == TokenType.MINUS)
+        {
+            match( TokenType.MINUS);
+        }
+        else if ( lookahead.getType() == TokenType.OR)
+        {
+            match( TokenType.OR); //need to add OR here?
+        }
+        else {
+            error( "Addop");
+        }
+    }
+
+    /**
+     *
+     */
+    public void mulop() {
+        if( lookahead.getType() == TokenType.MULTI)
+        {
+            match( TokenType.MULTI);
+        }
+        else if( lookahead.getType() == TokenType.FSLASH)
+        {
+            match( TokenType.FSLASH);
+        }
+        else if( lookahead.getType() == TokenType.DIV)
+        {
+            match( TokenType.DIV);
+        }
+        else if( lookahead.getType() == TokenType.MOD)
+        {
+            match( TokenType.MOD);
+        }
+        else if( lookahead.getType() == TokenType.AND)
+        {
+            match( TokenType.AND);
+        }
+        else {
+            error( "Mulop");
+        }
+    }
+
+    /**
+     * Creating the assignop method stated in the grammar.
+     * According to the micro pascal grammar, the ASSIGN token type
+     * stands for the assignop := token.
+     */
+    public void assignop()
+    {
+        if(lookahead.getType() == TokenType.ASSIGN)
+        {
+            match(TokenType.ASSIGN);
+        }
+        else
+        {
+            error("Assignop");
+        }
+    }
+
+    /**
+     *
+     *
+     * @param expected - This is the expected token type.
+     */
+    public void match( TokenType expected) {
+        System.out.println("match( " + expected + ")");
+        if( this.lookahead.getType() == expected) {
+            try {
+                this.lookahead = scanner.nextToken();
+                if( this.lookahead == null) {
+                    this.lookahead = new Token( "End of File", null);
+                }
+            } catch (IOException ex) {
+                error( "Scanner exception");
+            }
+        }
+        else {
+            error("Match of " + expected + " found " + this.lookahead.getType()
+                    + " instead.");
+        }
+    }
+
+    /**
+     * A custom error method for the parser. When the parser errors, an
+     * error message is printed out and the program is exited. The error
+     * message contains the error message string from the specific
+     * function, the line number the error was on, and the column number.
+     * @param message - The error message that is printed by using the
+     * error strings from different functions.
+     */
+    public void error( String message) {
+//        System.out.println( "Error " + message + " at line " +
+//                this.scanner.getLine() + " column " +
+//                this.scanner.getColumn());
+        //System.exit( 1);
+        throw new RuntimeException(message);
+    }
+
+    /**
+     * Checks to see if the token from the input string is a mulop token
+     * or not and returns a boolean expression if it's true or false.
+     * @param token - The lookahead token to check against from
+     * the input string.
+     * @return - If the token is a mulop it will be returned, otherwise
+     * it's false and nothing will be returned.
+     */
+    private boolean isMulop( Token token) {
+        boolean answer = false;
+        if( token.getType() == TokenType.MULTI ||
+                token.getType() == TokenType.FSLASH ||
+                token.getType() == TokenType.DIV ||
+                token.getType() == TokenType.MOD ||
+                token.getType() == TokenType.AND) {
+            answer = true;
+        }
+        return answer;
+    }
+
+
+    /**
+     * Checks to see if the token from the input string is a relop token
+     * or not and returns a boolean expression if it's true or false.
+     * @param token - The lookahead token to check against from the
+     * input string.
+     * @return - If the token is a relop it will be returned, otherwise
+     * it's false and nothing will be returned.
+     */
+    private boolean isRelop( Token token)
+    {
+        boolean answer = false;
+        if(token.getType() == TokenType.EQUIV ||
+                token.getType() == TokenType.NOTEQUAL ||
+                token.getType() == TokenType.LTHAN ||
+                token.getType() == TokenType.GTHAN ||
+                token.getType() == TokenType.LTHANEQUAL ||
+                token.getType() == TokenType.GTHANEQUAL)
+        {
+            answer = true;
+        }
+        return answer;
     }
 
 }
