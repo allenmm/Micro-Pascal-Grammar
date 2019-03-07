12162df352baec07ac888eed38d3ab0eef6f52d3
Marissa Allen <allenmm@augsburg.edu>
2019-03-07 13:31:25 -0600

    Created the KindEnum class for the SymbolTable.

---
+++ src/parser/KindEnum.java
@@ -0,0 +1,11 @@
+package parser;
+
+/**
+ * A class that contains the different kinds for every SymbolTable type.
+ *
+ * @author Marissa Allen
+ */
+public enum KindEnum
+{
+    PROGRAM_NAME, VAR_NAME, ARRAY_NAME, FUNCTION_NAME, PROCEDURE_NAME
+}
