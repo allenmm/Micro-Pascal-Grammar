d6f76ba95cc5e89e511997b313ea95b37859e80a
Marissa Allen <allenmm@augsburg.edu>
2019-03-07 13:32:46 -0600

    Added the addVarName method to the SymbolTable.

--- src/parser/SymbolTable.java
+++ src/parser/SymbolTable.java
@@ -7,7 +7,15 @@ public class SymbolTable
 {
 
     private HashMap<String, SymbolData> symbols = new HashMap<>();
-    
+
+
+    public void addVarName(String name)
+    {
+        if (!symbols.containsKey(name))
+        {
+            symbols.put(name, new SymbolData(name, KindEnum.VAR_NAME));
+        }
+    }
 
     private class SymbolData
     {
