8f5cd1294e7dcfd77e0a8232a7bf20828aca1252
Marissa Allen <allenmm@augsburg.edu>
2019-03-10 00:27:31 -0600

    Updated SymbolTable class javadocs.

--- src/parser/SymbolTable.java
+++ src/parser/SymbolTable.java
@@ -6,7 +6,8 @@ import java.util.HashMap;
  * This program creates a symbol table that is implemented as a hash map
  * to store information about every pascal identifier added to the symbol
  * table. The symbol table will store identifier information such as the
- * character string (or lexeme), its datatype, and its kind.
+ * character string (or lexeme), its datatype, and its kind. The kind of
+ * identifier can be a program, variable, array, function, or procedure.
  *
  * @author Marissa Allen
  */
