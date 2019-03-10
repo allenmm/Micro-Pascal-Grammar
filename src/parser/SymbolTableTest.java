952ae1b6a7cec86ad449aa5b25e9ccb651c500e8
Marissa Allen <allenmm@augsburg.edu>
2019-03-09 23:05:08 -0600

    Created a test for the isProcedureName method and added javadocs and comments to it. Also added class javadocs.

---
+++ src/parser/SymbolTableTest.java
@@ -0,0 +1,44 @@
+package parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * A JUnit Java Program that illustrates adding a symbol/identifier to the
+ * Symbol Table and checking to see if it is either a program, variable,
+ * function, or procedure identifier.
+ *
+ * @author Marissa Allen
+ */
+public class SymbolTableTest
+{
+    /**
+     * This method uses JUnit to test the isProcedureName method from the
+     * SymbolTable class by testing to see if the identifier is a
+     * procedure name and was properly added to the Symbol Table.
+     */
+    @Test
+    public void testIsProcedureName()
+    {
+        System.out.println("\n" + "#########################" + "\n" +
+                "#  Test IsProcedureName #" + "\n" +
+                "#########################" + "\n");
+        SymbolTable symbol = new SymbolTable();
+        symbol.addProcedureName("foo");
+        /* Positive test to see if the identifier was added to the
+         Symbol Table */
+        assertTrue(symbol.isProcedureName("foo"));
+        System.out.println("Passed, it is a procedure name.");
+        //Testing to make sure hash map added the correct identifier.
+        assertFalse(symbol.isProcedureName("fooz"));
+        System.out.println("Passed, variable name not recognized " +
+                "because it wasn't added to the SymbolTable.");
+        /* Negative test to see if a procedure name is accidentally
+         added as a variable name identifier.*/
+        assertFalse(symbol.isVarName("foo"));
+        System.out.println("Passed, variable name not recognized " +
+                "because it wasn't added to the SymbolTable.");
+    }
+
+
+}
