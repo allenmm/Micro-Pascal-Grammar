9b3e4592c331b48a767ba7ecc1d829d668783759
Marissa Allen <allenmm@augsburg.edu>
2019-03-09 20:41:26 -0600

    Added comments and javadocs and comments to the isArrayName method. Changed some of the javadoc wording for the isFunctionName method.

--- src/parser/SymbolTable.java
+++ src/parser/SymbolTable.java
@@ -83,9 +83,20 @@ public class SymbolTable
         return false;
     }
 
+    /**
+     * Checks to see if the identifier name exists and if it is an
+     * array name.
+     *
+     * @param name - The symbol name that is being checked.
+     * @return - A true will be returned if the name exists in the
+     * Symbol Table and if it's an array. A false will be returned if
+     * the name doesn't exist in the Symbol Table or it is not an
+     * array.
+     */
     public boolean isArrayName(String name)
     {
         SymbolData s = symbols.get(name);
+        //If an array name key exists and the kind is an array name
         if (s != null && s.kind == KindEnum.ARRAY_NAME)
         {
             return true;
@@ -95,10 +106,10 @@ public class SymbolTable
     }
 
     /**
-     * Checks to see if the Symbol Table name exists and if it is a
+     * Checks to see if the identifier name exists and if it is a
      * function name.
      *
-     * @param name - The Symbol Table name that is being checked.
+     * @param name - The symbol name that is being checked.
      * @return - A true will be returned if the name exists in the
      * Symbol Table and if it's a function. A false will be returned if
      * the name doesn't exist in the Symbol Table or it is not a
