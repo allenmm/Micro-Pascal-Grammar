d775a60f08e4bb36896970972d0a4c3952ce9f75
Marissa Allen <allenmm@augsburg.edu>
2019-03-17 15:06:59 -0500

    Made progress on the ParserTest. It now prints out the declaration names and the assignment name and value.

--- src/parser/ParserTest.java
+++ src/parser/ParserTest.java
@@ -4,7 +4,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import scanner.TokenType;
 import syntaxtree.*;
+
+import java.util.function.Function;
 
 /**
  *
@@ -14,23 +17,52 @@ public class ParserTest
 {
 
     /**
-     * JUnit test for a pascal program. 
+     * JUnit test for the indentedToString of a pascal program.
      */
     @Test
     public void programTest() {
 
-        String test = "program sample;\n" +
-                "var dollars, yen, bitcoins: integer;\n" +
-                "\n" +
-                "begin\n" +
-                "  dollars := 1000000;\n" +
-                "  yen := dollars * 110;\n" +
-                "  bitcoins := dollars / 3900\n" +
-                "end\n" +
-                ".";
-        Parser parser = new Parser( test, false);
-        ProgramNode actual = parser.program();
-        String actualString = actual.indentedToString( 0);
+        ProgramNode pn = new ProgramNode("sample");
+        DeclarationsNode dn = new DeclarationsNode();
+        pn.setVariables(dn);
+        VariableNode vn = new VariableNode("dollars");
+        VariableNode vn2 = new VariableNode("yen");
+        VariableNode vn3 = new VariableNode("bitcoins");
+        dn.addVariable(vn);
+        dn.addVariable(vn2);
+        dn.addVariable(vn3);
+        SubProgramDeclarationsNode sdn = new SubProgramDeclarationsNode();
+        pn.setFunctions(sdn);
+        CompoundStatementNode csn = new CompoundStatementNode();
+        pn.setMain(csn);
+        AssignmentStatementNode asn = new AssignmentStatementNode();
+        asn.setLvalue(vn);
+        ValueNode valueNode = new ValueNode("1000000");
+        asn.setExpression(valueNode);
+
+//        AssignmentStatementNode asn2 = new AssignmentStatementNode();
+//        asn2.setLvalue(vn2);
+//        OperationNode op = new OperationNode(TokenType.MULTI);
+//        ValueNode valueNode2 = new ValueNode(op);
+//        asn.setExpression(valueNode2);
+
+        csn.addStatement(asn);
+
+
+        String actual = pn.indentedToString(0);
+
+//        String test = "program sample;\n" +
+//                "var dollars, yen, bitcoins: integer;\n" +
+//                "\n" +
+//                "begin\n" +
+//                "  dollars := 1000000;\n" +
+//                "  yen := dollars * 110;\n" +
+//                "  bitcoins := dollars / 3900\n" +
+//                "end\n" +
+//                ".";
+//        Parser parser = new Parser( test, false);
+//        ProgramNode actual = parser.program();
+//        String actualString = actual.indentedToString( 0);
         String expectedString =
                 "Program: sample\n" +
                         "|-- Declarations\n" +
@@ -52,6 +84,6 @@ public class ParserTest
                         "|-- --- --- Operation: DIVIDE\n" +
                         "|-- --- --- --- Name: dollars\n" +
                         "|-- --- --- --- Value: 3900\n";
-        assertEquals( expectedString, actualString);
+        assertEquals( expectedString, actual);
     }
 }
