ae59b0a0f1834770e4a330decf34d23cdc7dbf14
Marissa Allen <allenmm@augsburg.edu>
2019-03-17 00:43:11 -0500

    Added the SubProgramNode class that returns the indentedToString method.

---
+++ src/syntaxtree/SubProgramNode.java
@@ -0,0 +1,13 @@
+package syntaxtree;
+
+/**
+ *
+ * @author Marissa Allen
+ */
+public class SubProgramNode
+{
+    public String indentedToString(int i)
+    {
+     return indentedToString(i);
+    }
+}
