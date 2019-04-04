6c3b54645587feccd026c176da22efcf7dacd646
Marissa Allen <allenmm@augsburg.edu>
2019-04-04 13:03:04 -0500

    Added the simple_expression method test which passes a test string into the parser and compares the generated indentedToString. And updated the factor method's javadocs.

--- src/parser/ParserTest.java
+++ src/parser/ParserTest.java
@@ -19,9 +19,9 @@ public class ParserTest
 
     /**
      * This method uses JUnit to test the factor method from the
-     * Parser class by testing to see if the current token matches
-     * the expected token type in the factor method. This is a text
-     * string test.
+     * Parser class by testing to see if the indentedToString from the
+     * call to the factor method matches the expected indentedToString.
+     * This is a text string test.
      */
     @Test
     public void testFactor()
@@ -38,7 +38,37 @@ public class ParserTest
         assertEquals(expected, actual);
         System.out.println("Parsed a factor");
         System.out.println("The factor for 87654321 is: ");
-        System.out.println( actual.indentedToString( 0));
+        System.out.println(actual);
 
     }
+
+    /**
+     * This method uses JUnit to test the simple_expression method from
+     * the Parser class by testing to see if the indentedToString from the
+     * call to the simple_expression method matches the expected
+     * indentedToString. This is a text string test.
+     */
+    @Test
+    public void testSimple_expression()
+    {
+        System.out.println("\n" + "##############################" +
+                "\n" + "#   Test simple_expression   #" + "\n" +
+                "##############################" + "\n");
+        String test = "34 + 17 * 7";
+        Parser instance = new Parser
+                (test, false);
+        ExpressionNode expressionNode = instance.simple_expression();
+        String expected = "Operation: PLUS\n" +
+                "|-- Value: 34\n" +
+                "|-- Operation: MULTI\n" +
+                "|-- --- Value: 17\n" +
+                "|-- --- Value: 7\n";
+        String actual = expressionNode.indentedToString(0);
+        assertEquals(expected, actual);
+        System.out.println("Parsed a simple_expression");
+        System.out.println("The simple_expression for 34 + 17 * 7 is: ");
+        System.out.println(actual);
+    }
+
+
 }
