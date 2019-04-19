78e1f3d41f0bcbefc5b4c93780aedf7abebf306b
Marissa Allen <allenmm@augsburg.edu>
2019-04-19 18:22:59 -0500

    Updated midterm.pas for a better testing file.

--- src/parser/midterm.pas
+++ src/parser/midterm.pas
@@ -1,6 +1,12 @@
-program foo ; var declarations : integer ;
+program foo ;
+var declarations : integer ;
 procedure fooz (identifier : integer) ;
 var declarations2 : integer ;
-begin read ( foo2 ) end ;
-begin write(foo + fooz *  foo3) end
+begin
+read ( declarations );
+write(declarations + fooz * declarations)
+end
+;
+begin
+end
 .
