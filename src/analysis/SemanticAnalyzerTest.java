12ed3393b587356f867f49040345743d49ccc962
Marissa Allen <allenmm@augsburg.edu>
2019-04-21 22:31:46 -0500

    Added a new negative test to the goodToGo method.

--- src/analysis/SemanticAnalyzerTest.java
+++ src/analysis/SemanticAnalyzerTest.java
@@ -17,10 +17,14 @@ public class SemanticAnalyzerTest
 
     /* This method uses JUnit to test the goodToGo method from the
      * SemanticAnalyzer class by using a negative test to make sure
-     * that all variables are declared before they are used. */
+     * that all variables are declared before they are used.
+     */
     @Test
     public void testGoodToGo()
     {
+        System.out.println("\n" + "#########################" + "\n" +
+                "#     Test goodToGo     #" + "\n" +
+                "#########################" + "\n");
         Parser parser = new Parser("fi", false);
         SymbolTable st = new SymbolTable();
         st.addVarName("fi", null);
@@ -30,6 +34,17 @@ public class SemanticAnalyzerTest
         assertFalse(analyze.goodToGo());
         System.out.println("Passed, variable name not declared " +
                 "because it has no type.");
+
+        parser = new Parser("fi - 5", false);
+        test = parser.expression();
+        st = new SymbolTable();
+        st.addVarName("fi", null);
+        analyze = new SemanticAnalyzer(null, st);
+        analyze.assignExpressionType(test);
+        assertFalse(analyze.goodToGo());
+        System.out.println("Passed, variable name not declared " +
+                "because it has no type.");
+
     }
 
 }
