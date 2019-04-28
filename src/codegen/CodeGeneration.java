bc3b33325ac6030f85723374056feb854d908ccc
Marissa Allen <allenmm@augsburg.edu>
2019-04-28 13:28:59 -0500

    Updated the class javadocs.

--- src/codegen/CodeGeneration.java
+++ src/codegen/CodeGeneration.java
@@ -7,8 +7,11 @@ import syntaxtree.*;
 import java.util.ArrayList;
 
 /**
- * This class takes in a ProgramNode and a symbol table and creates
- *
+ * This class takes in a syntax tree and a symbol table as its input and
+ * returns a string of MIPS assembly language code as its output.
+ * It does this by outlining a blueprint of the assembly code for the
+ * overall program and generating code to fill in the missing parts of
+ * the blueprint using nodes from the syntax tree.
  *
  * @author Marissa Allen
  */
